c6f9fd4f4ce97ea6397964947f3543dbd64cb9ff
Mikha'el <director@mikhaelbd.com>
2019-04-27 14:46:45 -0700

    Handler now working with API data

--- handler.js
+++ handler.js
@@ -1,39 +1,18 @@
 'use strict';
 
-import sanity from "../lib/sanity"
-import { success, failure } from './libs/response';
+const sanityClient = require('@sanity/client');
 
+const sanity = sanityClient({
+  // Find your project ID and dataset in `sanity.json` in your studio project
+  projectId: 'musclhjv',
+  dataset: 'production',
+  useCdn: true
+  // useCdn == true gives fast, cheap responses using a globally distributed cache.
+  // Set this to false if your application require the freshest possible
+  // data always (potentially slightly slower and a bit more expensive).
+})
 
 module.exports.readExperience = async (event, context, callback) => {
-  
-    // function httpGet() {
-    //     return new Promise(((resolve, reject) => {
-    //       var options = {
-    //           host: 'api.icndb.com',
-    //           port: 443,
-    //           path: '/jokes/random',
-    //           method: 'GET',
-    //       };
-          
-    //       const request = https.request(options, (response) => {
-    //         response.setEncoding('utf8');
-    //         let returnData = '';
-      
-    //         response.on('data', (chunk) => {
-    //           returnData += chunk;
-    //         });
-      
-    //         response.on('end', () => {
-    //           resolve(JSON.parse(returnData));
-    //         });
-      
-    //         response.on('error', (error) => {
-    //           reject(error);
-    //         });
-    //       });
-    //       request.end();
-    //     }));
-    //   }
 
       async function sanityQuery(schema) {
         const query = `*[_type == '${schema}']`
@@ -41,31 +20,29 @@ module.exports.readExperience = async (event, context, callback) => {
         return links;
       }
 
+      const result = await sanityQuery('productExperienceSchema')
+      await console.log(result);
 
-    function createResponse(body) {
-        const audio = body
+    
         const response = {
             version: '1.0',
             response: {
             outputSpeech: {
                 type: 'PlainText',
-                text: `The first step is to ${audio}`,
+                text: `${result[0].title} ${result[0].overview[0].children[0].text}.`,
             },
             shouldEndSession: false,
             },
         }
-        return response;
-        }
+        
 
-  try {
-    const result = await sanityQuery('productSchema')
-    console.log('Result: ', result);
+//   try {
+//     const result = await sanityQuery('productSchema')
+//     console.log('Result', result);
     
-    return success ({ result })
-} catch (error) {
-    console.log('Error:', e)
-    return failure({ status: false })
-}
-
-//   callback(null, response);
+    
+// } catch (error) {
+//     console.log('Error:', error)
+// }
+  callback(null, response)
 };
